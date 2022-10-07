d619e1f7bd1e2cd141eec3611cdfdd066cfd9c31
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2022-10-08 08:09:33 +0900

    remove unused sources

--- FluentCeVIOWrapper.Common/Interfaces/ITalkerComponentArray.cs
+++
@@ -1,9 +0,0 @@
-namespace FluentCeVIOWrapper.Common.Talk;
-internal interface ITalkerComponentArray
-{
-	int Length { get; }
-
-	ITalkerComponent At(int index);
-	ITalkerComponent ByName(string name);
-	ITalkerComponentArray Duplicate();
-}
