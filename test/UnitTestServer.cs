30f9f2c4471ba3de5b5a97ef5213d92d4f2c3fdc
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2023-09-30 00:20:56 +0900

    test: update tests

--- test/UnitTestServer.cs
+++ test/UnitTestServer.cs
@@ -155,7 +155,7 @@ public class UnitTestServer : IClassFixture<UnitTestAwakeServer>, IDisposable
 
 	//[Trait("Category", "ServiceMethod")]
 	//[Fact]
-	public async Task StartDirectAsync(){
+	private async Task StartDirectAsync(){
 		HostStartResult? result = null;
 
 		client.MessageReceived += async (_, args) =>
@@ -257,7 +257,6 @@ public class UnitTestServer : IClassFixture<UnitTestAwakeServer>, IDisposable
 		//await Task.Delay(100);
 		output.WriteLine($"{nameof(StartWrapperAsync)}: delay");
 
-		result.Should().NotBeNull();
 		result.Should().Be(
 			HostStartResult.Succeeded,
 			$"ホストが正しく起動していない：{result.ToString()}");
@@ -296,7 +295,8 @@ public class UnitTestServer : IClassFixture<UnitTestAwakeServer>, IDisposable
 
 		var result = await fcw.GetCastAsync();
 
-		Assert.True(cast == result);
+		result.Should().NotBeNull();
+		result.Should().Be(cast);
 	}
 
 	[Fact]
@@ -529,13 +529,13 @@ public class UnitTestServer : IClassFixture<UnitTestAwakeServer>, IDisposable
 	}
 
 	[Theory]
-	//[InlineData("さとうささら", Product.CeVIO_AI, "CTNV-JPF-A" )]
-	[InlineData("さとうささら", Product.CeVIO_CS, "A" )]
+	[InlineData("さとうささら", Product.CeVIO_AI, "CTNV-JPF-A" )]
+	//[InlineData("さとうささら", Product.CeVIO_CS, "A" )]
 	public async void GetCastIdAsync(
 		string name, Product product, string id
 	){
 		var fcw = await FluentCeVIO
-			.FactoryAsync(product: product);
+			.FactoryAsync(FluentCeVIO.PipeName,product);
 		await fcw
 			.CreateParam()
 			.Cast(name)
@@ -547,5 +547,25 @@ public class UnitTestServer : IClassFixture<UnitTestAwakeServer>, IDisposable
 
 		var getId = await fcw.GetCastIdAsync(name);
 		getId.Should().Be(id);
+	}
+
+	[Theory]
+	[InlineData(Product.CeVIO_AI)]
+	public async void SpeakLongTextAsync(
+		Product product
+	)
+	{
+		var fcw = await FluentCeVIO
+			.FactoryAsync(FluentCeVIO.PipeName,product);
+
+		var text = @"
+吾輩は猫である。名前はまだ無い。
+　どこで生れたかとんと見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。吾輩はここで始めて人間というものを見た。しかもあとで聞くとそれは書生という人間中で一番獰悪な種族であったそうだ。この書生というのは時々我々を捕えて煮て食うという話である。しかしその当時は何という考もなかったから別段恐しいとも思わなかった。ただ彼の掌に載せられてスーと持ち上げられた時何だかフワフワした感じがあったばかりである。掌の上で少し落ちついて書生の顔を見たのがいわゆる人間というものの見始であろう。この時妙なものだと思った感じが今でも残っている。第一毛をもって装飾されべきはずの顔がつるつるしてまるで薬缶だ。その後猫にもだいぶ逢ったがこんな片輪には一度も出会わした事がない。のみならず顔の真中があまりに突起している。そうしてその穴の中から時々ぷうぷうと煙を吹く。どうも咽せぽくて実に弱った。これが人間の飲む煙草というものである事はようやくこの頃知った。
+この書生の掌の裏でしばらくはよい心持に坐っておったが、しばらくすると非常な速力で運転し始めた。書生が動くのか自分だけが動くのか分らないが無暗に眼が廻る。胸が悪くなる。到底助からないと思っていると、どさりと音がして眼から火が出た。それまでは記憶しているがあとは何の事やらいくら考え出そうとしても分らない。
+　ふと気が付いて見ると書生はいない。たくさんおった兄弟が一疋も見えぬ。肝心の母親さえ姿を隠してしまった。その上今までの所とは違って無暗に明るい。眼を明いていられぬくらいだ。はてな何でも容子がおかしいと、のそのそ這い出して見ると非常に痛い。吾輩は藁の上から急に笹原の中へ棄てられたのである。
+";
+		var result = await fcw.SpeakAsync(text);
+
+		result.Should().Be(true);
 	}
 }
